fcdecb3d639cfdb0a0de4d7c5915f8ecec187557
todest <todest@qq.com>
2022-06-17 16:34:18 +0800

    Changes

--- src/main/java/cn/todest/mcutils/mixin/McUtilsMixin.java
+++ src/main/java/cn/todest/mcutils/mixin/McUtilsMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package cn.todest.mcutils.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import cn.todest.mcutils.McUtilsMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class McUtilsMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		McUtilsMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
